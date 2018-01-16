f8a6725e4c989a4a74726f2cafb9a49d8e60baa0
aunn <aunn_it08@hotmail.com>
2018-01-16 13:40:38 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 9999;
+app.use('/', function (request, response) {
+    response.json({ message: 'Hello world' })
+})
+app.listen(port)
+console.log('Server is running on port: ' + port)
